78ba937f5d196b54676471dc0b8147128ada294f
pranavipathuri <pp42496@srcs.org>
2022-04-11 17:47:16 -0500

    4/11/22

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,10 +11,12 @@ public class NewBehaviourScript : MonoBehaviour
     }
 
     public float horizontalInput;
+    public float speed = 10.0f;
 
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
