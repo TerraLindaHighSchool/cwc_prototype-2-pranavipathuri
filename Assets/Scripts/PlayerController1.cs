34af3dd9d1f214fb5850243e10218c35865b828e
pranavipathuri <pp42496@srcs.org>
2022-04-12 17:42:28 -0500

    2.2.5 tutorials complete

---
+++ Assets/Scripts/PlayerController1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController1 : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
+    public GameObject projectilePrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //uses keys to move player
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        
+        //keep the player in bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+       
+        //checking for a spacebar press
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        
+    }
+}
