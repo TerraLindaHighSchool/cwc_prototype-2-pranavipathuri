3b39a17c2a0c2d3caa56d7c39c99496074d3e271
pranavipathuri <pp42496@srcs.org>
2022-04-18 17:48:17 -0500

    2.4 W.I.P.

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -11,10 +11,15 @@ public class DestroyOutOfBounds : MonoBehaviour
     }
 
     private float topBound = 30;
+    private float lowerBound = -10;
     // Update is called once per frame
     void Update()
     {
         if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
         {
             Destroy(gameObject);
         }
